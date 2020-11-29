8ecd2f756d376d0710312fa504e540a1c65fe051
Per Jansson <per.r.jansson@gmail.com>
2020-11-29 17:30:46 +0200

    Switch back to real

--- lighthouserc.js
+++ lighthouserc.js
@@ -7,7 +7,7 @@ module.exports = {
     },
     assert: {
       assertions: {
-        'categories:performance': ['error', { minScore: 1 }],
+        'categories:performance': ['error', { minScore: 0.95 }],
         // 'categories:accessibility': ['error', { minScore: 1 }],
       },
     },
