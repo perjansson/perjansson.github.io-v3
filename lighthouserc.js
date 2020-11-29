916642ba63de739cd8619540889948b413d797f8
Per Jansson <per.r.jansson@gmail.com>
2020-11-29 12:33:28 +0200

    no start server command

--- lighthouserc.js
+++ lighthouserc.js
@@ -2,7 +2,7 @@ module.exports = {
   ci: {
     collect: {
       numberOfRuns: 3,
-      startServerCommand: 'make run-ci',
+      // startServerCommand: 'make run-ci',
       url: ['http://localhost:3000'],
     },
     assert: {
