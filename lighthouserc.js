8e0bb4a1e7069805e270c350a07cc75bdc1ba848
Per Jansson <per.r.jansson@gmail.com>
2020-11-29 17:19:37 +0200

    Disable a11y perf score

--- lighthouserc.js
+++ lighthouserc.js
@@ -8,7 +8,7 @@ module.exports = {
     assert: {
       assertions: {
         'categories:performance': ['error', { minScore: 1 }],
-        'categories:accessibility': ['error', { minScore: 1 }],
+        // 'categories:accessibility': ['error', { minScore: 1 }],
       },
     },
     upload: {
