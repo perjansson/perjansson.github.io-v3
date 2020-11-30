b665d4218f7dc9017c712fed6eeca917bd3acaad
Per Jansson <per.r.jansson@gmail.com>
2020-11-30 22:13:24 +0200

    Revert "0.98 score"
    This reverts commit 4187bbd4b8ffcdd045ff7a091b8d75ac641a514c.

--- lighthouserc.js
+++ lighthouserc.js
@@ -7,8 +7,8 @@ module.exports = {
     },
     assert: {
       assertions: {
-        'categories:performance': ['error', { minScore: 0.98 }],
-        'categories:accessibility': ['error', { minScore: 0.98 }],
+        'categories:performance': ['error', { minScore: 1 }],
+        'categories:accessibility': ['error', { minScore: 1 }],
       },
     },
     upload: {
