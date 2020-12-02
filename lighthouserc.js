cd14a279e6ec780eda16b85750a05bc9ba2d610c
Per Jansson <per.r.jansson@gmail.com>
2020-12-02 21:32:09 +0200

    Disable some LH assertions

--- lighthouserc.js
+++ lighthouserc.js
@@ -6,14 +6,14 @@ module.exports = {
       url: ['http://localhost:5000'],
     },
     assert: {
-      preset: 'lighthouse:recommended',
+      // preset: 'lighthouse:recommended',
       assertions: {
         'categories:performance': ['error', { minScore: 1 }],
         'categories:accessibility': ['error', { minScore: 1 }],
-        'categories:best-practices': ['error', { minScore: 1 }],
-        'categories:seo': ['error', { minScore: 1 }],
-        'first-contentful-paint': ['error', { maxNumericValue: 1000 }],
-        interactive: ['error', { maxNumericValue: 1000 }],
+        // 'categories:best-practices': ['error', { minScore: 1 }],
+        // 'categories:seo': ['error', { minScore: 1 }],
+        // 'first-contentful-paint': ['error', { maxNumericValue: 1000 }],
+        // interactive: ['error', { maxNumericValue: 1000 }],
       },
     },
     upload: {
