492a20ac01fb67fa4ffbb8e81ef5fc8a865f660d
Per Jansson <per.r.jansson@gmail.com>
2021-01-02 20:27:10 +0200

    LH a11y at 1

--- lighthouserc.js
+++ lighthouserc.js
@@ -8,7 +8,7 @@ module.exports = {
       // preset: 'lighthouse:recommended',
       assertions: {
         'categories:performance': ['error', { minScore: 0.95 }],
-        'categories:accessibility': ['error', { minScore: 0.98 }],
+        'categories:accessibility': ['error', { minScore: 1 }],
         // 'categories:best-practices': ['error', { minScore: 1 }],
         // 'categories:seo': ['error', { minScore: 1 }],
         // 'first-contentful-paint': ['error', { maxNumericValue: 1000 }],
