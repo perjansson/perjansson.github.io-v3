4607bfcb6209bb4366e6d3729bd904d1d8bf5545
Per Jansson <per.jansson@embark-studios.com>
2022-01-03 08:25:40 +0200

    Configurate custom image loader

--- next.config.js
+++ next.config.js
@@ -3,6 +3,7 @@ const withPWA = require('next-pwa')
 module.exports = withPWA({
   images: {
     domains: ['images.ctfassets.net'],
+    loader: 'custom',
   },
   pwa: {
     disable: process.env.NODE_ENV === 'development',
